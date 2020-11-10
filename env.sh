5f04b45d69c8263c7c3ed7d840baab9d89171cd0
Whale <jesse2061@outlook.com>
2020-11-10 10:03:46 +0800

    Update env.sh

--- env.sh
+++ env.sh
@@ -20,7 +20,7 @@ function _get_release() {
     fi
 }
 unix_release=$(if [ "$unix_s" = "Linux" ]; then cat /etc/issue | grep -v '^$' | awk '{print $1}'; elif [ "$unix_s" = "Darwin" ]; then sw_vers | grep ProductName | awk '{print $2" "$3" "$4}'; fi)
-
+unix_release=$(_get_release)
 trap 'onCtrlC' INT
 function onCtrlC() {
     echo ""
